e016b67b97cba569ec981f198ebb3814897efbfe
vinosa75 <vinosa75@gmail.com>
2022-11-02 08:41:24 +0530

    permissions

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 poetry install
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
+pip install celery
 
 python manage.py collectstatic --no-input
 python manage.py migrate
